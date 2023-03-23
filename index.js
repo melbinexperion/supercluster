8d9edab2d23a54775e9cbad747d609eeff473322
Melbin Mathew <melbin97@hotmail.com>
2023-03-23 17:33:22 +0530

    modified range function

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const defaultOptions = {
     radius: 40,   // cluster radius in pixels
     extent: 512,  // tile extent (radius is calculated relative to it)
     nodeSize: 64, // size of the KD-tree leaf node, affects performance
-    log: false,   // whether to log timing info
+    log: true,   // whether to log timing info
 
     // whether to generate numeric ids for input features (in vector tiles)
     generateId: false,
@@ -81,13 +81,57 @@ export default class Supercluster {
         }
 
         const tree = this.trees[this._limitZoom(zoom)];
-        const ids = tree.range(lngX(minLng), latY(maxLat), lngX(maxLng), latY(minLat));
+        const ids = this.range(lngX(minLng), latY(maxLat), lngX(maxLng), latY(minLat));
         const clusters = [];
         for (const id of ids) {
             const c = tree.points[id];
             clusters.push(c.numPoints ? getClusterJSON(c) : this.points[c.index]);
         }
         return clusters;
+    }
+
+    range(ids, coords, minX, minY, maxX, maxY, nodeSize) {
+        console.log("inside range funciton")
+        var stack = [0, ids.length - 1, 0];
+        var result = [];
+        var x, y;
+    
+        while (stack.length) {
+            var axis = stack.pop();
+            var right = stack.pop();
+            var left = stack.pop();
+    
+            if (right - left <= nodeSize) {
+                for (var i = left; i <= right; i++) {
+                    x = coords[2 * i];
+                    y = coords[2 * i + 1];
+                    if (x >= minX && x <= maxX && y >= minY && y <= maxY) { result.push(ids[i]); }
+                }
+                continue;
+            }
+    
+            var m = Math.floor((left + right) / 2);
+    
+            x = coords[2 * m];
+            y = coords[2 * m + 1];
+    
+            if (x >= minX && x <= maxX && y >= minY && y <= maxY) { result.push(ids[m]); }
+    
+            var nextAxis = (axis + 1) % 2;
+    
+            if (axis === 0 ? minX <= x : minY <= y) {
+                stack.push(left);
+                stack.push(m - 1);
+                stack.push(nextAxis);
+            }
+            if (axis === 0 ? maxX >= x : maxY >= y) {
+                stack.push(m + 1);
+                stack.push(right);
+                stack.push(nextAxis);
+            }
+        }
+    
+        return result;
     }
 
     getChildren(clusterId) {
